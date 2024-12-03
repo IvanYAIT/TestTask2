1922657d2b4243fcfeaa6b916060cef1c266c7f6
IvanYAIT <YAkushikia20@st.ithub.ru>
2024-12-03 13:26:08 +0300

    Task1 complete

--- TestTask2/Assets/_Source/Task1/Bootstrapper.cs
+++ TestTask2/Assets/_Source/Task1/Bootstrapper.cs
@@ -1,18 +1,60 @@
+using Cysharp.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
 
-public class Bootstrapper : MonoBehaviour
+namespace Task1
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Bootstrapper : MonoBehaviour
     {
-        
-    }
+        private static string URL = "https://rc-today.ru/UserFiles/Image/a3/fd/kartina_po_nomeram_molly_prischepa_kraski_vechera_40h50_24_tsveta_kh0617_5eb2a3691b37a_6798_big.jpg";
+        private static string SPRITE_NAME = "Image";
+        private static string SCENE_NAME = "SampleScene";
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [SerializeField] private SceneView view;
+
+        private void Start()
+        {
+            LoadWebImage(URL);
+            LoadResourceImage(SPRITE_NAME);
+            LoadScene(SCENE_NAME);
+        }
+
+        private async UniTask LoadWebImage(string url)
+        {
+            var uwr = (await UnityWebRequestTexture.GetTexture(url).SendWebRequest().ToUniTask(Progress.Create<float>(x => view.SetWebProgress(x))));
+            if (uwr.result != UnityWebRequest.Result.Success)
+                return;
+
+            var texture = DownloadHandlerTexture.GetContent(uwr);
+            Sprite sprite = Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f), 100.0f);
+            view.SetWebImage(sprite);
+        }
+
+        private async UniTask LoadResourceImage(string spriteName)
+        {
+            var sprite = await Resources.LoadAsync<Sprite>(spriteName).ToUniTask(Progress.Create<float>(x => view.SetResourceProgress(x)));
+
+            view.SetResourceImage((Sprite) sprite);
+        }
+
+        private async UniTask LoadScene(string sceneName)
+        {
+            AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+            operation.allowSceneActivation = false;
+
+            await operation.ToUniTask(Progress.Create<float>( x => { 
+                if(x == 0.9f)
+                {
+                    x = 1;
+                    view.SceneBtn.interactable = true;
+                    view.SceneBtn.onClick.AddListener(()=> operation.allowSceneActivation=true);
+                }
+                view.SetSceneProgress(x);
+            }));
+        }
     }
 }
---
+++ TestTask2/Assets/_Source/Task1/SceneView.cs
@@ -0,0 +1,45 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Task1
+{
+    public class SceneView : MonoBehaviour
+    {
+        [SerializeField] private Image webImg;
+        [SerializeField] private Image resourceImg;
+        [SerializeField] private Slider webSlider;
+        [SerializeField] private Slider resourceSlider;
+        [SerializeField] private Slider sceneSlider;
+        [SerializeField] private TextMeshProUGUI webText;
+        [SerializeField] private TextMeshProUGUI resourceText;
+        [SerializeField] private TextMeshProUGUI sceneText;
+        [SerializeField] private Button sceneBtn;
+
+        public Button SceneBtn => sceneBtn;
+
+        public void SetWebImage(Sprite img) =>
+            webImg.sprite = img;
+
+        public void SetWebProgress(float value)
+        {
+            webSlider.value = value;
+            webText.text = $"Загрузка(Web): {value*100:F0}/100%";
+        }
+
+        public void SetResourceImage(Sprite img) =>
+            resourceImg.sprite = img;
+
+        public void SetResourceProgress(float value)
+        {
+            resourceSlider.value = value;
+            resourceText.text = $"Загрузка(Resource): {value * 100:F0}/100%";
+        }
+
+        public void SetSceneProgress(float value)
+        {
+            sceneText.text = $"След. сцена: {value * 100:F0}/100%";
+            sceneSlider.value = value;
+        }
+    }
+}
